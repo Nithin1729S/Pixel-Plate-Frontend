6d7fbf2dcfef2f2e1cc7fef8f91f92395d642507
nithin <sureshnithin1729@gmail.com>
2024-06-23 14:08:10 +0530

    search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
 import MainNav from "./MainNav";
-
+import resLogo from "../images/favicon2.png"
 const Header = () => {
-  return (
-    <div className="border-b-2 border-b-orange-500 py-6">
-        <div className="container mx-auto flex justify-between items-center">
-                <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>
-                        Foodie
-                </Link>
+    return (
+        <div className="border-b-2 border-b-orange-500 py-6">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-center space-x-2">
+                    <img src={resLogo} alt="logo" style={{height:"64px", width:'64px'}} />
+                    <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>
+                        Pixel Plate
+                    </Link>
+                </div>
+
                 <div className="md:hidden">
-                    <MobileNav/>
+                    <MobileNav />
                 </div>
                 <div className="hidden md:block">
-                    <MainNav/>
+                    <MainNav />
                 </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header;
